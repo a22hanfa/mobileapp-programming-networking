9ebf67d7cf63ca61b03ea4b5824bf5f281513955
Hannes <12446977+glazedegg@users.noreply.github.com>
2023-05-01 16:53:32 +0200

    - Add RecyclerView to MainActivity's layout file - Create Mountain model class with appropriate fields - Add RecyclerView.Adapter and ArrayList<Mountain> in MainActivity - Set JSON_URL to fetch mountain data

---
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -0,0 +1,51 @@
+package com.example.networking;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Mountain {
+
+    private String Location;
+    private String Name;
+    @SerializedName("size")
+    private int Height;
+
+    public Mountain() {
+
+        this.Location = Location;
+        this.Name = Name;
+        this.Height = Height;
+    }
+
+    public String getLocation() {
+        return Location;
+    }
+
+    public void setLocation(String location) {
+        Location = location;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getHeight() {
+        return Height;
+    }
+
+    public void setHeight(int height) {
+        Height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Mountain{" +
+                "Location='" + Location + '\'' +
+                ", Name='" + Name + '\'' +
+                ", Height=" + Height +
+                '}';
+    }
+}
